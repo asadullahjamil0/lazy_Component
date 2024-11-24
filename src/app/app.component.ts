340607ee3ddba71110b25f762d256abcb66923c7
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-11-24 21:19:20 +0500

    Lazy Loading component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+import { AdminlistComponent } from './adminlist/adminlist.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lazy_Component';
+  constructor(private containerView: ViewContainerRef,
+    private cfr: ComponentFactoryResolver) { }
+  async loadAdmin() {
+    this.containerView.clear();
+    const { AdminlistComponent } = await import('./adminlist/adminlist.component');
+    this.containerView.createComponent(
+      this.cfr.resolveComponentFactory(AdminlistComponent)
+    )
+  }
+  async loadUser() {
+    this.containerView.clear();
+    const { UserlistComponent } = await import('./userlist/userlist.component');
+    this.containerView.createComponent(
+      this.cfr.resolveComponentFactory(UserlistComponent)
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminlistComponent } from './adminlist/adminlist.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminlistComponent,
+    UserlistComponent
   ],
   imports: [
     BrowserModule,
